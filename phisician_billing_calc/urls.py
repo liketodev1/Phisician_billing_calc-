ba56b86b1350260fbc53d371bcfcc2cc20fbb645
Artavazd Mnatsakanyan <artomnats1996@gmail.com>
2020-03-03 12:25:37 +0400

    Working version with backend and frontend

--- phisician_billing_calc/urls.py
+++ phisician_billing_calc/urls.py
@@ -1,7 +1,7 @@
 """phisician_billing_calc URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,11 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
-    path('users/',  include('django.contrib.auth.urls')),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', include('frontend.urls')),
 ]
