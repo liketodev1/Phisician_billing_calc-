0e7650b91ec2e8326be0846f980be180f7626c41
Galust <mr.galust@gamil.com>
2020-04-30 00:50:33 +0400

    Refactor the pagination

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,26 +1,8 @@
-import React, { useState, useRef } from 'react';
+import React from 'react';
 import './Pagination.scss';
 
 const Pagination = ({ cardsPerPage, totalCards, paginate }) => {
 
-  const paginationContainer = useRef(null);
-
-  const [toggle, setToggle] = useState(true);
-
-  const setActiveClass = () => {
-
-// Get all buttons with class="btn" inside the container
-    let paginationItems = paginationContainer.current.getElementsByClassName('page-item');
-
-// Loop through the buttons and add the active class to the current/clicked button
-    for (let i = 0; i < paginationItems.length; i++) {
-      paginationItems[i].addEventListener('click', function () {
-        let currentItem = document.getElementsByClassName('active');
-        currentItem[0].className = currentItem[0].className.replace(' active', '');
-        this.className += ' active';
-      });
-    }
-  };
 
   const pageNumbers = [];
 
@@ -32,15 +14,12 @@ const Pagination = ({ cardsPerPage, totalCards, paginate }) => {
     e.preventDefault();
     paginate(number);
 
-    let paginationItems = paginationContainer.current.getElementsByClassName('page-item');
-    console.log('paginationItems', paginationItems);
 
-    setActiveClass();
   };
 
   return (
     <nav className='Pagination container'>
-      <ul className="pagination" ref={ paginationContainer }>
+      <ul className="pagination">
         { pageNumbers.map(number => (
           <li key={ number } className='page-item'>
             <a href="!#" className='page-link' onClick={ e => paginateHandler(e, number) }>
